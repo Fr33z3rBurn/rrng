d9a26da4a336b0dd5003b69f48b0ed83ab33b561
Tim Freese <freese.tim@gmail.com>
2022-06-13 15:50:31 -0500

    loop for ingredients

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,24 +12,29 @@ export default class HomePage extends React.Component {
 
   getRandomRecipe = (e) => {
     Api.GetRandomRecipe().then((res) => {
-      this.setState({
-        recipe: res.meals[0],
-      });
-      //this.combineIngredients(this.state.recipe);
+      this.setupRecipe(res.meals[0]);
     });
   };
 
-  combineIngredients = (meal) => {
+  setupRecipe = (meal) => {
     const ingredientsArray = [];
     for (let i = 1; i <= 20; i++) {
-      if (this.state.recipe["strIngredient" + i]) {
-        alert("here");
+      if (meal["strIngredient" + i] !== "") {
+        var ingredientSet = [
+          {
+            id: i,
+            amount: meal["strMeasure" + i],
+            ingredient: meal["strIngredient" + i],
+          },
+        ];
+        ingredientsArray.push(ingredientSet);
       } else {
         // Stop if no more ingredients
         break;
       }
     }
-    this.setState(this.state.ingredients, ingredientsArray);
+    this.setState({ ingredients: ingredientsArray });
+    this.setState({ recipe: meal });
   };
 
   render() {
@@ -69,128 +74,17 @@ export default class HomePage extends React.Component {
                 </tr>
               </thead>
               <tbody>
-                {this.state.recipe.strIngredient1 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure1}</th>
-                    <th>{this.state.recipe.strIngredient1}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient2 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure2}</th>
-                    <th>{this.state.recipe.strIngredient2}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient3 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure3}</th>
-                    <th>{this.state.recipe.strIngredient3}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient4 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure4}</th>
-                    <th>{this.state.recipe.strIngredient4}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient5 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure5}</th>
-                    <th>{this.state.recipe.strIngredient5}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient6 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure6}</th>
-                    <th>{this.state.recipe.strIngredient6}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient7 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure7}</th>
-                    <th>{this.state.recipe.strIngredient7}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient8 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure8}</th>
-                    <th>{this.state.recipe.strIngredient8}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient9 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure9}</th>
-                    <th>{this.state.recipe.strIngredient9}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient10 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure10}</th>
-                    <th>{this.state.recipe.strIngredient10}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient11 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure11}</th>
-                    <th>{this.state.recipe.strIngredient11}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient12 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure12}</th>
-                    <th>{this.state.recipe.strIngredient12}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient13 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure13}</th>
-                    <th>{this.state.recipe.strIngredient13}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient14 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure14}</th>
-                    <th>{this.state.recipe.strIngredient14}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient15 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure15}</th>
-                    <th>{this.state.recipe.strIngredient15}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient16 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure16}</th>
-                    <th>{this.state.recipe.strIngredient16}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient17 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure17}</th>
-                    <th>{this.state.recipe.strIngredient17}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient18 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure18}</th>
-                    <th>{this.state.recipe.strIngredient18}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient19 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure19}</th>
-                    <th>{this.state.recipe.strIngredient19}</th>
-                  </tr>
-                )}
-                {this.state.recipe.strIngredient20 !== "" && (
-                  <tr>
-                    <th>{this.state.recipe.strMeasure20}20</th>
-                    <th>{this.state.recipe.strIngredient20}20</th>
-                  </tr>
+                {this.state.ingredients.map((nested) =>
+                  nested.map((element) => (
+                    <tr key={element.id}>
+                      <td>{element.amount}</td>
+                      <td>{element.ingredient}</td>
+                    </tr>
+                  ))
                 )}
               </tbody>
             </Table>
+
             <p>{this.state.recipe.strInstructions}</p>
 
             {this.state.recipe.strYoutube !== "" && (
