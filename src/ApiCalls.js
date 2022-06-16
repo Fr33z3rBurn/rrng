bf4e3d1a8d1cbb97d2cf9ce7014e4138aba8a0da
Tim Freese <freese.tim@gmail.com>
2022-06-15 22:25:35 -0500

    clean up

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -3,7 +3,7 @@ const Api = Object.create(
   {
     GetRandomRecipe: {
       value: () => {
-        return fetch("https://www.themealdb.com/api/json/v2/9973533/random.php").then((res) => res.json());
+        return fetch("https://www.themealdb.com/api/json/v1/1/random.php").then((res) => res.json());
       },
     },
   }
